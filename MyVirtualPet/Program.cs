c809c4d2949dcb86c61db549d9715f0e35f46037
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-08-24 10:42:27 -0300

    Refatorando o  Jogo

--- MyVirtualPet/Program.cs
+++ MyVirtualPet/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,60 +11,8 @@ namespace MyVirtualPet
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void LerArquivoTexto(string nome, string nomeDono, ref float alimentado, ref float limpo, ref float feliz)
         {
-            /*
-             * O jogo mantém os dados do pet mesmo ao finalizar o game
-             * Inicia o programa via prompt e coleto o nome do pet
-             * Coleta os dados de alimento, limpo e feliz em um arquivo
-             * De tempos em tempo vamos diminuir o valor de uma caracteristica do pet
-             */
-
-            string nome = "";
-            string nomeDono = "";
-            string entrada = "";
-            string resposta = "";
-
-            float alimentado = 100;
-            float limpo = 100;
-            float feliz = 100;
-
-            //diminuir os valores das características do pet
-            int caracteristica = 0;
-            Random rand = new Random();
-
-            //fales do pet
-
-            string[] frases = new string[10];
-
-            frases[0] = "Nossa, o dia foi muito legal, comi o sofá!";
-            frases[1] = "Não contava com minha astúcia!";
-            frases[3] = "Foi sem querer querendo!";
-            frases[4] = "Que saudades, passei o dia todo esperando você chegar";
-            frases[5] = "Hoje assistir um filme muito bom!";
-            frases[6] = "Você não vai com minha cara!";
-            frases[7] = "Oh! E agora, quem poderá mim ajudar!";
-            frases[8] = "Não gostaria de tomar uma xicara de café?";
-            frases[9] = "Isso mim dá coisa!";
-            
-            Console.WriteLine("My Pet Virtual");
-            
-            //coleta de dados
-            //entrada de dados
-            if (args.Length > 0)
-            {
-                nome = args[0];
-            }
-            else
-            {
-                Console.Write("Qual é o nome do seu pet? ");
-                nome = Console.ReadLine();
-            }
-
-            Console.Write("Oi, qual o nome do meu dono? ");
-            nomeDono = Console.ReadLine();
-            Console.WriteLine("Legal estava com muita saudade de você, " + nomeDono + "!");
-            //coletar os dados do pet no arquivo texto
             String dir = Environment.CurrentDirectory + "\\";
             String file = dir + nome + nomeDono + ".txt";
             if (File.Exists(file))
@@ -84,33 +33,115 @@ namespace MyVirtualPet
                     feliz = 100;
                 }
             }
-            //o programam principal
-            entrada = "sim";
-            while(entrada.ToLower() != "nada" && alimentado > 0 && limpo > 0 && feliz > 0)
-            {
-                Console.Clear();
-                Console.WriteLine("Alimentado.: " + alimentado);
-                Console.WriteLine("Limpo.: " + limpo);
-                Console.WriteLine("Feliz.: " + feliz);
-                //alterar o status do pet
-                // 0 - perde alimento / 1 - perde limpeza / 2 - perde limpeza
-                caracteristica = rand.Next(3);
-                switch (caracteristica)
-                {
-                    case 0:
-                        alimentado -= rand.Next(10,40);
-                        break;
-                    case 1:
-                        limpo -= rand.Next(10,40);
-                        break;
-                    case 2:
-                        feliz -= rand.Next(10,40);
-                        break;
+        }
 
+        static void GravarArquivoTexto(string nome, string nomeDono, float alimentado, float limpo, float feliz)
+        {
+            String fileContent = nome + Environment.NewLine;
+            fileContent += nomeDono + Environment.NewLine; 
+            fileContent += alimentado + Environment.NewLine;
+            fileContent += limpo + Environment.NewLine;
+            fileContent += feliz + Environment.NewLine;
+
+            //gravei no arquivo texto
+            String dir = Environment.CurrentDirectory + "\\";
+            String file = dir + nome + nomeDono + ".txt";
+            File.WriteAllText(file, fileContent);
+        }
+
+        static string Falar()
+        {
+            //fales do pet
+
+            string[] frases = new string[10];
+
+            frases[0] = "Nossa, o dia foi muito legal, comi o sofá!";
+            frases[1] = "Não contava com minha astúcia!";
+            frases[3] = "Foi sem querer querendo!";
+            frases[4] = "Que saudades, passei o dia todo esperando você chegar";
+            frases[5] = "Hoje assistir um filme muito bom!";
+            frases[6] = "Você não vai com minha cara!";
+            frases[7] = "Oh! E agora, quem poderá mim ajudar!";
+            frases[8] = "Não gostaria de tomar uma xicara de café?";
+            frases[9] = "Isso mim dá coisa!";
+
+            Random rand = new Random();
+            string frase = frases[rand.Next(frases.Length)];
+
+            return frase;
+        }
+
+        static string Interagir(string nomeDono, ref float alimentado, ref float limpo, ref float feliz) 
+        {
+            Random rand = new Random();
+            Console.WriteLine("{0}, o que vamos fazer hoje? ", nomeDono);
+            Console.Write("Brincar/Comer/Banho/nada.: ");
+            string entrada = Console.ReadLine().ToLower();
+
+            switch (entrada)
+            {
+                case "brincar": feliz += rand.Next(30); break;
+                case "Comer": alimentado += rand.Next(30); break;
+                case "banho": limpo += rand.Next(30); break;
+            }
+            if (feliz > 100) feliz = 100;
+            if (limpo > 100) limpo = 100;
+            if (alimentado > 100) alimentado = 100;
+
+            return entrada;
+        }
+
+        static void AtualizarStatus(ref float alimentado, ref float limpo, ref float feliz)
+        {
+            //alterar o status do pet
+            // 0 - perde alimento / 1 - perde limpeza / 2 - perde limpeza
+
+            Random rand = new Random();
+            int caracteristica = rand.Next(3);
+            switch (caracteristica)
+            {
+                case 0: alimentado -= rand.Next(10, 40); break;
+                case 1: limpo -= rand.Next(10, 40); break;
+                case 2: feliz -= rand.Next(10, 40); break;
+
+            }
+        }
+
+        static void ExibirStatus(float alimentado, float limpo, float feliz, int tipo)
+        {
+            if(tipo == 0)
+            {
+                Console.WriteLine("Status do Pet");
+                Console.WriteLine("Alimentado.: {0}", alimentado);
+                Console.WriteLine("Limpo.: {0}", limpo);
+                Console.WriteLine("Feliz.: {0}", feliz);
+            }
+            if(tipo == 1)
+            {
+                if (alimentado > 40 && alimentado < 60)
+                {
+                    Console.WriteLine("Eu estou faminto!!!!!");
+                    Console.WriteLine("Nada melhor do que um comidinha...");
                 }
-                
-                Console.WriteLine("Olá! {0}", nomeDono);
-                Console.WriteLine(frases[rand.Next(frases.Length)]);
+                if (limpo > 40 && limpo < 60)
+                {
+                    Console.WriteLine("Nossa, estou meio sujinho!!!!!");
+                    Console.WriteLine("Nada melhor do que um banho...");
+                }
+                if (feliz > 40 && feliz < 60)
+                {
+                    Console.WriteLine("Fiquei em casa o dia todo!!!!!");
+                    Console.WriteLine("Nada melhor do que brincar...");
+                }
+            }
+            if(tipo == 2)
+            {
+                Console.WriteLine("Status do Pet");
+                Console.WriteLine("Alimentado.: {0}", alimentado);
+                Console.WriteLine("Limpo.: {0}", limpo);
+                Console.WriteLine("Feliz.: {0}", feliz);
+
+                Console.WriteLine("Nível de Felicidade");
 
                 if (alimentado > 40 && alimentado < 60)
                 {
@@ -127,34 +158,62 @@ namespace MyVirtualPet
                     Console.WriteLine("Fiquei em casa o dia todo!!!!!");
                     Console.WriteLine("Nada melhor do que brincar...");
                 }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string nome = "";
+            string nomeDono = "";
+            string entrada = "";
+            
+            //Status do pet
+            float alimentado = 100;
+            float limpo = 100;
+            float feliz = 100;
+
+            //diminuir os valores das características do pet
+                       
+            
+            Console.WriteLine("My Pet Virtual");
+            
+            //coleta de dados
+            //entrada de dados
+            if (args.Length > 0)
+            {
+                nome = args[0];
+            }
+            else
+            {
+                Console.Write("Qual é o nome do seu pet? ");
+                nome = Console.ReadLine();
+            }
+
+            Console.Write("Oi, qual o nome do meu dono? ");
+            nomeDono = Console.ReadLine();
+            Console.WriteLine("Legal estava com muita saudade de você, " + nomeDono + "!");
+
+            //coletar os dados do pet no arquivo texto
+            LerArquivoTexto(nome, nomeDono, ref alimentado, ref limpo, ref  feliz);
+
+            //o programam principal
+            //brinca com o PET
+            entrada = "sim"; 
+            while(entrada.ToLower() != "nada" && alimentado > 0 && limpo > 0 && feliz > 0)
+            {
+                              
+                AtualizarStatus(ref alimentado, ref limpo, ref feliz);
+
+                Console.Clear();
+                Console.WriteLine("Olá! {0}", nomeDono);
+                Console.WriteLine(Falar());
+
+                ExibirStatus(alimentado, limpo, feliz, 1);
 
                 Thread.Sleep(3000);
                 Console.Clear();
 
+                entrada = Interagir(nomeDono, ref alimentado, ref limpo, ref feliz);
                 
-                
-                Console.WriteLine("{0}, o que vamos fazer hoje? ", nomeDono);
-                Console.Write("Brincar/Comer/Banho/nada.: ");
-                resposta = Console.ReadLine().ToLower();
-
-                switch (resposta)
-                {
-                    case "brincar":
-                        feliz += rand.Next(30);
-                        break;
-                    case "Comer":
-                        alimentado += rand.Next(30);
-                        break;
-                    case "banho":
-                        limpo += rand.Next(30);
-                        break;
-                    case "nada":
-                        entrada = "nada";
-                        break;
-                }
-                if (feliz > 100) feliz = 100;
-                if (limpo > 100) limpo = 100;
-                if (alimentado > 100) alimentado = 100;
 
             }
             //saiu do jogo
@@ -172,13 +231,8 @@ namespace MyVirtualPet
             Console.WriteLine("Até outro dia!!!");
             //armazenar os dados
             //criei os dados
-            String fileContent = nome + Environment.NewLine;
-            fileContent += nomeDono + Environment.NewLine;
-            fileContent += alimentado + Environment.NewLine;
-            fileContent += limpo + Environment.NewLine;
-            fileContent += feliz + Environment.NewLine;
-            //gravei no arquivo texto
-            File.WriteAllText(file, fileContent);
+
+            GravarArquivoTexto(nome, nomeDono, alimentado, limpo, feliz);
 
             Console.ReadKey();
         }
